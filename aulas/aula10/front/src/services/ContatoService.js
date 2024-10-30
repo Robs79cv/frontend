9fa3bbf6308670e480a3ebf88b0b6f2bf956a34c
unknown <2224290095@ACADEMICO.IESB.BR>
2024-10-29 21:20:03 -0300

    Atualizacao aulas

---
+++ aulas/aula10/front/src/services/ContatoService.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+const url = import.meta.env.VITE_API_URL;
+
+function carregarContatos() {
+  return axios
+    .get(url)
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+}
+
+function criarContato(contato) {
+  return axios
+    .post(url, contato)
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+
+  // try {
+  //   const response = await axios.post(url, contato)
+  //   return { sucesso: true, dados: response.data }
+  // } catch (error)  {
+  //   return { sucesso: false, mensagem: error.message }
+  // }
+}
+
+function atualizarContato(contato) {
+  return axios.put(`${url}/${contato.id}`, {
+    nome: contato.nome,
+    telefone: contato.telefone,
+  }).then(response => {return {sucesso: true, dados: response.data}})
+  .catch(error => {return {sucesso: false, mensagem: error.mensage}});
+}
+
+function removerContato(id) {
+  return axios
+    .delete(`${url}/${id}`) // http://localhost:3000/contatos/id
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+}
+
+function obterContato(id) {
+  return axios
+    .get(`${url}/${id}`)
+    .then((response) => {
+      return { sucesso: true, dados: response.data };
+    })
+    .catch((error) => {
+      return { sucesso: false, mensagem: error.message };
+    });
+}
+
+export { carregarContatos, criarContato, removerContato, obterContato, atualizarContato };
