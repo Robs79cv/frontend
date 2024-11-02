43c1f59b08d8434d561892c8c736617cea1a183a
unknown <2224290095@ACADEMICO.IESB.BR>
2024-11-01 21:16:51 -0300

    Adicionando arquivos da pratica10

--- Práticas/pratica10/front/src/App.jsx
+++ Práticas/pratica10/front/src/App.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Listar from "./pages/Listar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Novo from "./pages/Novo";
+import Listar from "./pages/Listar";
 import Editar from "./pages/Editar";
+import Error404 from "./pages/Error404";
+
 
 function App() {
   return (
+  
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Listar />} />
         <Route path="/novo" element={<Novo />} />
         <Route path="/editar/:id" element={<Editar />} />
-        <Route path="/editar/:id" element={<Editar />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
