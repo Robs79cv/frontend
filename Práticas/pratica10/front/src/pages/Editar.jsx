cae20c1a67d99f3f749431749541be0132009fb6
unknown <robsontec.viana79@gmail.com>
2024-11-02 17:03:33 -0300

    atualização prati10

--- Práticas/pratica10/front/src/pages/Editar.jsx
+++ Práticas/pratica10/front/src/pages/Editar.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Cabecalho from "../components/Cabecalho";
+import Conteudo from "../components/Conteudo";
+import Formulario from "./Formulario";
+
 function Editar() {
-    return (
-      <></>
-    );
-  }
-  
-  export default Editar;
-  
+  useEffect(() => {
+    carregar;
+  }, []);
+  const [contato, setContato] = useState({});
+  const [erro, setErro] = useState("");
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const carregar = async () => {
+    const resposta = await buscarUm(id);
+    if (resposta.sucesso) {
+      setContato(resposta.dados);
+    } else {
+      setErro(resposta.mensagem);
+    }
+    const trataEnviar = async (data) => {
+      const resposta = await atualizar({ id, ...data });
+      if (resposta.sucesso) {
+        navigate("/");
+      } else {
+        setErro(resposta.mensagem);
+      }
+    };
+  };
+
+  return (
+    <>
+      <Cabecalho />
+      <Conteudo>
+        <h2>Editar Contato</h2>
+        <Formulario trataEnviar={trataEnviar} valores={contato} />
+        {erro && <p>{erro}</p>}
+      </Conteudo>
+    </>
+  );
+}
+
+export default Editar;
